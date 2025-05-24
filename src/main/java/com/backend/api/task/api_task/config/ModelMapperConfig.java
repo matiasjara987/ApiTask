43d42d33651a6a44826c2f81695ed79bd99d6db5
Matias jara <matiasjats@gmail.com>
2025-05-24 01:58:20 -0400

    create configuration bean

---
+++ src/main/java/com/backend/api/task/api_task/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.backend.api.task.api_task.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
