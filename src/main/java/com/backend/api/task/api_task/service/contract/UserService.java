054db9adfe2e0b0940042dee385951d34e048a98
Matias jara <matiasjats@gmail.com>
2025-05-26 19:32:37 -0400

    reafactor imp

--- src/main/java/com/backend/api/task/api_task/service/contract/UserService.java
+++ src/main/java/com/backend/api/task/api_task/service/contract/UserService.java
@@ -4,12 +4,11 @@ package com.backend.api.task.api_task.service.contract;
 
 
 import com.backend.api.task.api_task.dto.userdto.*;
-import com.backend.api.task.api_task.model.User;
 
 public interface UserService {
     
     public UserResponseDTO findByEmail(String email);
     public UserResponseDTO findById(Long id);
     public void deleteUser(Long id);
-    UserResponseDTO save(User user);
+    UserResponseDTO save(UserRequestDTO UserRequestDTO);
 }
