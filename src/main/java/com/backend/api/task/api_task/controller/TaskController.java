b072f0eb1dad1634d09cf59ce3b5b4bb244c9bc4
Matias jara <matiasjats@gmail.com>
2025-05-28 09:04:36 -0400

    Create controller task

--- src/main/java/com/backend/api/task/api_task/controller/TaskController.java
+++ src/main/java/com/backend/api/task/api_task/controller/TaskController.java
@@ -1,5 +1,22 @@
 package com.backend.api.task.api_task.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.backend.api.task.api_task.dto.taskdto.TaskResponseDTO;
+import com.backend.api.task.api_task.service.contract.TaskService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/tasks")
+@RequiredArgsConstructor
 public class TaskController {
     
+    private TaskService taskService;
+
+    @GetMapping("/{id}")
+    public TaskResponseDTO getTaskById(@PathVariable Long id){
+        return taskService.findbyid(id);
+    }
+
 }
